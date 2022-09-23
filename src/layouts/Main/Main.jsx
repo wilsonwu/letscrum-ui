4e525b94c2e82290029394b83045496cac6393c6
Wilson Wu <iwilsonwu@gmail.com>
2022-09-23 21:07:18 +0800

    > fix width

--- src/layouts/Main/Main.jsx
+++ src/layouts/Main/Main.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Row, Col } from 'react-bootstrap'
+import { Row, Col, Container } from 'react-bootstrap'
 import { Header, Footer, SideMenu } from "../../components";
 import PropTypes from "prop-types";
 
@@ -7,14 +7,16 @@ export const Main = ({ children }) => {
   return (
     <>
       <Header />
-      <Row>
-        <Col sm={2} className="pe-0">
-          <SideMenu />
-        </Col>
-        <Col sm={10} className="px-0">
-          {children}
-        </Col>
-      </Row>
+        <Container fluid>
+          <Row >
+            <Col sm={2} className="pe-0">
+              <SideMenu />
+            </Col>
+            <Col sm={10} className="px-0">
+              {children}
+            </Col>
+          </Row>
+        </Container>
       <Footer />
     </>
   )
