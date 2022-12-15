f6b1242894eca7614e053328136465fa6a4b9f14
ilynnluo <60775749+ilynnluo@users.noreply.github.com>
2022-12-15 12:21:18 -0400

    + SideMenu UI Style

--- src/components/sideNav/SideNav.tsx
+++ src/components/sideNav/SideNav.tsx
@@ -1,11 +1,40 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import { Box, MenuList, MenuItem, ListItemText, Avatar, Typography } from '@mui/material'
+import { grey, green } from '@mui/material/colors'
+import { styled } from '@mui/system'
 
-export const SideNav: React.FC = () => {
+const SideNavList = styled(MenuList)({
+  width: '20rem',
+  height: '100vh',
+  paddingTop: '1rem',
+  borderRightWidth: '1px',
+  borderRightStyle: 'solid',
+  borderRightColor: grey[300]
+})
+
+const SideMenuItem = styled(MenuItem)({
+  padding: '.75rem',
+  backgroundColor: grey[100]
+})
+
+export const SideNav: React.FunctionComponent = () => {
   return (
-    <>
-      Here is SideNav
+    <Box sx={{ display: 'flex' }}>
+      <SideNavList>
+        <SideMenuItem>
+          <Avatar sx={{ width: '1.5rem', height: '1.5rem', bgcolor: green[800] }} variant="rounded">
+            <Typography sx={{ fontSize: '.875rem', fontWeight: 300 }}>
+              A
+            </Typography>
+          </Avatar>
+          <ListItemText sx={{ fontSize: '.875rem', fontWeight: 300 }}>Cut</ListItemText>
+        </SideMenuItem>
+        <MenuItem>
+          <ListItemText>Web Clipboard</ListItemText>
+        </MenuItem>
+      </SideNavList>
       <Outlet />
-    </>
+    </Box>
   )
 }
--- src/pages/organizationPage/OrganizationPage.tsx
+++ src/pages/organizationPage/OrganizationPage.tsx
@@ -1,10 +1,11 @@
+import { Box } from '@mui/material'
 import React from 'react'
 // import { MainLayout } from '../../layout/mainLayout/MainLayout'
 
 export const OrganizationPage: React.FunctionComponent = () => {
   return (
-    <>
+    <Box sx={{ width: '100%' }}>
       Here is the main content
-    </>
+    </Box>
   )
 }
