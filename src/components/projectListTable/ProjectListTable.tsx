039f4e1cf4599bd86f466cf608a3b57d71772379
ilynnluo <60775749+ilynnluo@users.noreply.github.com>
2022-12-20 16:35:09 -0400

    > upgrade Project Card UI

--- src/components/projectListTable/ProjectListTable.tsx
+++ src/components/projectListTable/ProjectListTable.tsx
@@ -1,12 +1,12 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useState } from 'react'
 import { Stack, Card, CardContent, Typography, CardActions, Avatar } from '@mui/material'
 import styled from '@emotion/styled'
-import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord'
-import { grey, red } from '@mui/material/colors'
+import { FiberManualRecord, FactCheck, AccountTree, RocketLaunch, Science, Widgets } from '@mui/icons-material'
+import { grey, red, teal, deepOrange, blue, deepPurple, pink } from '@mui/material/colors'
 import Grid from '@mui/material/Unstable_Grid2/Grid2'
 
 const TopProjectsWrapper = styled(Stack)({
-  width: '30%',
+  width: '100%',
   padding: '1rem 0'
 })
 const CardActionsWrapper = styled(CardActions)({
@@ -21,52 +21,88 @@ const IconWrapper = styled('div')({
   justifyContent: 'center',
   alignItems: 'center'
 })
-const FiberManualRecordIconStyle = styled(FiberManualRecordIcon)({
+const FiberManualRecordIcon = styled(FiberManualRecord)({
   color: grey[500],
   width: '.5rem',
   height: '.5rem'
 })
-const card = (
-  <>
-    <CardContent>
-      <Grid container>
-        <Grid sx={{ marginRight: '1rem' }}>
-          <Avatar sx={{ backgroundColor: red[900], width: '3rem', height: '3rem' }} variant="rounded">
-            I
-          </Avatar>
+export const ProjectListTable = (): ReactElement => {
+  const [hover, setHover] = useState<boolean>(false)
+  const handleMouseIn = (): void => setHover(true)
+  const handleMouseOut = (): void => setHover(false)
+  return (
+    <TopProjectsWrapper direction='row'>
+      <Grid container spacing={2} sx={{ flexGrow: 1, width: '100%' }}>
+        <Grid xs={4}>
+          <Card sx={{ minWidth: '18rem', height: '12rem' }}>
+            <CardContent sx={{ height: '7rem' }}>
+              <Stack direction='row'>
+                <Grid sx={{ width: '4rem' }}>
+                  <Avatar sx={{ backgroundColor: red[900], width: '3rem', height: '3rem' }} variant="rounded">
+                    I
+                  </Avatar>
+                </Grid>
+                <Grid sx={{ width: 'auto' }}>
+                  <Typography sx={{ fontSize: '1.125rem' }}>
+                    benevolent
+                  </Typography>
+                  <Typography sx={{ color: grey[400] }}>
+                    adjective
+                  </Typography>
+                </Grid>
+              </Stack>
+            </CardContent>
+            <CardActionsWrapper
+              onMouseEnter={handleMouseIn}
+              onMouseLeave={handleMouseOut}
+            >
+            {
+              hover
+                ? <>
+                <IconWrapper>
+                    <FactCheck sx={{ color: teal[500] }} />
+                  </IconWrapper>
+                  <IconWrapper>
+                    <AccountTree sx={{ color: deepOrange[700] }} />
+                  </IconWrapper>
+                  <IconWrapper>
+                    <RocketLaunch sx={{ color: blue[800] }} />
+                  </IconWrapper>
+                  <IconWrapper >
+                    <Science sx={{ color: deepPurple[600] }} />
+                  </IconWrapper>
+                  <IconWrapper>
+                    <Widgets sx={{ color: pink[500] }} />
+                  </IconWrapper>
+                </>
+                : <>
+                  <IconWrapper>
+                    <FiberManualRecordIcon />
+                  </IconWrapper>
+                  <IconWrapper>
+                    <FiberManualRecordIcon />
+                  </IconWrapper>
+                  <IconWrapper>
+                    <FiberManualRecordIcon />
+                  </IconWrapper>
+                  <IconWrapper>
+                    <FiberManualRecordIcon />
+                  </IconWrapper>
+                  <IconWrapper>
+                    <FiberManualRecordIcon />
+                  </IconWrapper>
+                </>
+            }
+            </CardActionsWrapper>
+          </Card>
         </Grid>
-        <Grid>
-          <Typography sx={{ fontSize: '1.125rem' }} component="div">
-            benevolent
-          </Typography>
-          <Typography sx={{ mb: 1.5 }} color="text.secondary">
-            adjective
-          </Typography>
+        <Grid xs={4}>
+          card
+        </Grid>
+        <Grid xs={4}>
+          card
         </Grid>
       </Grid>
-    </CardContent>
-    <CardActionsWrapper>
-      <IconWrapper>
-        <FiberManualRecordIconStyle />
-      </IconWrapper>
-      <IconWrapper>
-        <FiberManualRecordIconStyle />
-      </IconWrapper>
-      <IconWrapper>
-        <FiberManualRecordIconStyle />
-      </IconWrapper>
-      <IconWrapper>
-        <FiberManualRecordIconStyle />
-      </IconWrapper>
-    </CardActionsWrapper>
-  </>
-)
-export const ProjectListTable = (): ReactElement => {
-  return (
-    <TopProjectsWrapper sx={{ minWidth: 275 }}>
-      <Card>
-        {card}
-      </Card>
     </TopProjectsWrapper>
   )
 }
